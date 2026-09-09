f4d26b30179bd4d2a525de572e50cbea0fc7a2ff
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-09-09 14:52:00 +0530

    intialize the repo

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-blue-400'>This is shubham</h1>
+    </>
+  )
+}
+
+export default App
