12ba42ff19124585989848c88ada1c2c8a3328b4
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-09-11 00:00:57 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import DropZone from './components/DropZone'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+     const handleFiles = (files: File[]) => {
+    console.log(files);
+  };
+  
 
   return (
-    <>
+    <div>
      <h1 className='bg-blue-400'>This is shubham</h1>
-    </>
+      <DropZone onFilesSelected={handleFiles} />
+    </div>
   )
 }
 
